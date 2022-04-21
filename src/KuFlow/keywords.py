a87b4ca08ed0fc3c409dd6e837c801e2abaecc9a
Zebensui Méndez Corujo <zmencor@kuflow.com>
2022-04-21 11:14:42 +0100

    Polish

--- src/KuFlow/keywords.py
+++ src/KuFlow/keywords.py
@@ -26,7 +26,9 @@ import logging
 import kuflow_rest_client
 from kuflow_rest_client.api import task_api
 from kuflow_rest_client.model.delete_element_command import DeleteElementCommand
-from kuflow_rest_client.model.delete_element_document_command import DeleteElementDocumentCommand
+from kuflow_rest_client.model.delete_element_document_command import (
+    DeleteElementDocumentCommand,
+)
 from kuflow_rest_client.model.element_value_or_array_value import (
     ElementValueOrArrayValue,
 )
